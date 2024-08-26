7593fb961cbce5cad3cac6d032b6d327717413a8
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-26 14:20:35 +0300

    Implement basic arithmetic functions
    - Added `more(num)` function to return the input number incremented by 1.
    - Implemented `less(num)` function to subtract 1 from the input number (currently missing a return statement).
    - Created `add(num1, num2)` function to return the sum of two numbers.
    - Added `sub(num1, num2)` function to return the difference between the second and first numbers.

---
+++ more-or-less.js
@@ -0,0 +1,17 @@
+function more(num) {
+    return num + 1
+
+}
+
+function less(num) {
+    num - 1
+}
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+
+function sub(num1, num2) {
+    return num2 - num1
+}
