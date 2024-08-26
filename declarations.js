0e634e4e7d236bb126b4a7061bc7d59b40873d80
aaochieng <ochiengaoron@gmail.com>
2024-08-26 12:25:29 +0300

    Fix: Object freeze

--- declarations.js
+++ declarations.js
@@ -1,17 +1,19 @@
 const escapeStr = "`\\/\"\`\'";
-const arr = Object.freeze([4, '2'])
-const obj = Object.freeze({
+const arr = [4, '2']
+const obj = {
   str: "string",
   num: 1,
   bool: true,
   undef: undefined
-})
-
+}
 const nested = {
-  arr: Object.freeze([4, undefined, '2']),
-  obj: Object.freeze({
+  arr: [4, undefined, '2'],
+  obj: {
     str: "string value",
     num: 2,
     bool: false
-  })
+  }
 }
+Object.freeze(arr)
+Object.freeze(obj)
+Object.freeze(nested)
