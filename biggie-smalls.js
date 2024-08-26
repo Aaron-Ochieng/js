a9d7b6c91e49700edb22fb5c9e97d472dddcc028
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-26 17:42:13 +0300

    Add: fixed smallest number possible

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,6 +1,6 @@
-const smalls = Number.MIN_VALUE
+const smalls = Number.NEGATIVE_INFINITY
 const biggie = Number.POSITIVE_INFINITY
 
 
 // console.log(smalls)
-console.log(biggie+9)
+// console.log(smalls)
