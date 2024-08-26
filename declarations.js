6731a22e68a9519427adffe6bd40f40079acd1a6
aaochieng <ochiengaoron@gmail.com>
2024-08-26 11:56:11 +0300

    Declare complex data structures with arrays, objects, and nested objects
    - Added `escapeStr` constant with an escaped string value "`\\/`".
    - Created `arr` array containing a number and a string.
    - Defined `obj` object with properties `str`, `num`, `bool`, and `undef`, representing different data types.
    - Introduced `nested` object containing:
      - A frozen array `arr` with elements of different types, including `undefined`.
      - A frozen nested object `obx` with properties `str`, `num`, and `bool`.

---
+++ declarations.js
@@ -0,0 +1,20 @@
+const escapeStr = "`\\/'";
+const arr = [4,'2']
+const obj = {
+  str:"string",
+  num:1,
+  bool:true,
+  undef:undefined
+}
+
+const nested = {
+  arr : Object.freeze([4,undefined,'2']),
+  obx:Object.freeze({
+    str:"string value",
+    num:2,
+    bool:false
+  })
+}
+
+
+console.log(nested)
