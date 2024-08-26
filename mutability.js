b0095ed633aac43199b3d2dcfd5daa6d88b114ed
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-26 14:15:19 +0300

    Clone and modify the person object
        - Created `clone1` and `clone2` as shallow copies of the `person` object using the spread operator.
        - Defined `samePerson` as another shallow copy of the `person` object.
        - Updated the original `person` object by incrementing the `age` property by 1 and adding a new `country` property with the value "FR".

--- mutability.js
+++ mutability.js
@@ -1,6 +1,19 @@
+// const person = {
+//     name: "John",
+//     age: 30,
+//     country: "US"
+// };
+
+
 const clone1 = { ...person }
 const clone2 = { ...person }
-const samePerson = { ...person }
+const samePerson = person
 
-samePerson.age += 1
-samePerson.country = "FR"
+person.age += 1
+person.country = "FR"
+
+// console.log(person)
+// console.log(clone1)
+// console.log(clone2)
+// console.log(samePerson)
+// console.log(person)
