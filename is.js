adda94ac091e3da73eadf25be297d4ee44024137
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-27 15:06:37 +0300

    Fix: object

--- is.js
+++ is.js
@@ -6,7 +6,7 @@ is.str = function (value) { return typeof value === "string"; };
 is.bool = function (value) { return typeof value === "boolean"; };
 is.undef = function (value) { return typeof value === "undefined"; };
 is.def = function (value) { return typeof value !== "undefined"; };
-is.obj = function (value) { return value !== null && typeof value === "object"; };
+is.obj = function (value) { return value !== null && typeof value === "object" && !Array.isArray(value) };
 is.fun = function (value) { return typeof value === "function"; };
 is.truthy = function (value) { return !!value; };
 is.falsy = function (value) { return !value; };
