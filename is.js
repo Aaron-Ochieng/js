1091dbb03bd1abedc57cf7f76ba273fe50544817
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-27 13:51:26 +0300

    Fix: fixed is

--- is.js
+++ is.js
@@ -1,14 +1,12 @@
 // const is = {};
 
-
-is.num = function (value) { return typeof (value) === "number" }
-is.nan = function (value) { return typeof (value) != "number" }
-is.str = function (value) { return typeof (value) === "string" }
-is.bool = function (value) { return typeof (value) === "boolean" }
-is.undef = function (value) { return typeof (value) === "undefined" }
-is.def = function (value) { return typeof (value) != "undefined" }
-is.obj = function (value) { return typeof (value) === "object" || value === null }
-is.fun = function (value) { return typeof (value) === "function" }
-is.truthy = function (value) { return !!value }
-is.falsy = function (value) { return !value }
-// console.log(is.undef(is)) 
+is.num = function (value) { return typeof value === "number"; };
+is.nan = function (value) { return Number.isNaN(value); };
+is.str = function (value) { return typeof value === "string"; };
+is.bool = function (value) { return typeof value === "boolean"; };
+is.undef = function (value) { return typeof value === "undefined"; };
+is.def = function (value) { return typeof value !== "undefined"; };
+is.obj = function (value) { return value !== null && typeof value === "object"; };
+is.fun = function (value) { return typeof value === "function"; };
+is.truthy = function (value) { return !!value; };
+is.falsy = function (value) { return !value; };
