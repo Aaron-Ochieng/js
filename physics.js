e038c207d1c263ceb9f1f48a5376ead6023dce66
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-28 08:59:56 +0300

    Add: acceleratoin using time and distance

--- physics.js
+++ physics.js
@@ -4,8 +4,10 @@ function getAcceleration(obj) {
         return obj.f / obj.m;
     } else if (typeof obj.Δv === "number" && typeof obj.Δt === "number") {
         return obj.Δv / obj.Δt;
+    } else if (typeof obj.d === "number" && typeof obj.t === "number") {
+        return (obj.d * 2) / (obj.t * obj.t)
     }
     return "impossible";
 }
 
-// console.log(getAcceleration({ f: 10, m: 5 }))
+// console.log(getAcceleration({ d: 10, t: 2, Δv: 100 }))
