775a2edbe6cd5d5bbec134e93edfb485ba494160
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-28 08:31:33 +0300

    Fix: added Δt and Δv

--- physics.js
+++ physics.js
@@ -1,10 +1,10 @@
 function getAcceleration(obj) {
-    const { f, m, v, t } = obj;
+    const { f, m, Δv, Δt } = obj;
 
     if (f !== undefined && m !== undefined) {
         return f / m;
-    } else if (v !== undefined && t !== undefined) {
-        return v / t;
+    } else if (Δv !== undefined && Δt !== undefined) {
+        return Δv / Δt;
     } else {
         return "impossible";
     }
