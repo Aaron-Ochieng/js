9b463834e06a8ce4e86152c06eade0e37c6d970a
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-30 16:46:15 +0300

    Fix: drawing pyramid in the terminal

--- pyramid.js
+++ pyramid.js
@@ -1,21 +1,15 @@
-const triangle = (string, value) => {
+function pyramid(string, number) {
     let res = ""
-    for (let i = 1; i <= value; i++) {
-        if (i !== value) {
-            res += reprint(string, i) + "\n"
+    for (let i = 0; i < number; i++) {
+        let spaces = ' '.repeat((number - i - 1) * string.length)
+        let strings = string.repeat(i * 2 + 1)
+        if (i !== number - 1) {
+            res += spaces + strings + "\n"
         } else {
-            res += reprint(string, i)
+            res += spaces + strings
         }
     }
     return res
 }
 
-const reprint = (string, n) => {
-    let res = ""
-    for (let i = 0; i < n; i++) {
-        res += string
-    }
-    return res
-}
-
-// console.log(pyramid("{}", 12))
+console.log(pyramid("{}", 12))
