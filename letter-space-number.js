8ef0b7e580125690be49210c016533ac4b9c3031
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-30 17:10:34 +0300

    Fix: fixed the regular expression

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,8 +2,10 @@ function letterSpaceNumber(str) {
     const exprr = /([a-zA-Z]) (\d)(?!\d)/g;
     let matches = [];
     let match;
-    while ((match = regex.exec(str)) !== null) {
+    while ((match = exprr.exec(str)) !== null) {
         matches.push(`${match[1]} ${match[2]}`);
     }
     return matches;
 }
+
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
