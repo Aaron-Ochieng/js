61dc56d94aa9b12ba522d0bf01f27c14656f1b96
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-30 17:01:28 +0300

    Fix: using only regex(removed use of functions)

--- its-a-match.js
+++ its-a-match.js
@@ -1,24 +1,6 @@
-function normal(str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str.slice(i, i + 2) === "hi") return true
-    }
-    return false
-}
+const normal = /hi/;
+const begin = /^hi/;
+const end = /hi$/;
+const beginEnd = /^hi$/;
 
-function beginEnd(str) {
-    return str === "hi"
-}
-
-function end(str) {
-    if (str.length < 2) return false
-    return str.slice(str.length - 2, str.length) === "hi"
-}
-
-function begin(str) {
-    if (str.length < 2) return false
-    return str.slice(0, 2) === "hi"
-}
-
-
-
-console.log(begin("hellohihowareyou"))
+console.log(normal.test("hellohi"))
