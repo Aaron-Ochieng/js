3a4c60e3e1e43b302416a8a2a889246b2c2ae718
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-30 14:28:33 +0300

    Fix: removed code test functions

--- unbreakable.js
+++ unbreakable.js
@@ -59,9 +59,9 @@ function join(arr, by) {
     }
 }
 
-const arr1 = [1, 2, null, 4]
-const arr2 = [5, 6, 7, 6]
+// const arr1 = [1, 2, null, 4]
+// const arr2 = [5, 6, 7, 6]
 
-console.log(typeof arr1)
-console.log(arr1.join("*"))
-console.log(join(arr1, "*"))
+// console.log(typeof arr1)
+// console.log(arr1.join("*"))
+// console.log(join(arr1, "*"))
