d031a6a7c629291b1aafd829880bff92e811e564
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-03 09:46:19 +0300

    Fix: isvalid function

--- date-is.js
+++ date-is.js
@@ -1,8 +1,8 @@
 const isValid = (date) => {
-    if (new Date(date).toDateString() === "Invalid Date") {
+    if (new Date(date).toString() === "Invalid Date") {
         return false
     }
-    if (!(date instanceof Date) && !isNaN(date.getTime())) {
+    if (!(date instanceof Date) && typeof date !== 'number') {
         return false;
     }
 
@@ -12,7 +12,3 @@ const isAfter = (date_1, date_2) => Date.parse(date_1) > Date.parse(date_2)
 const isBefore = (date_1, date_2) => !isAfter(date_1, date_2)
 const isFuture = (date) => date > new Date()
 const isPast = (date) => !isFuture(date)
-// console.log(isFuture(new Date('08-08-2030')))
-// console.log((new Date('1995-12-17T03:24:00').getTime()))
-// console.log(isValid(''))
-// console.log(isValid(new Date('1995-12-17T03:24:00')))
