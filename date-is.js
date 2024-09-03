fc4fb8804e65821ca6fc7ea0c04d72604e095089
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-03 09:51:12 +0300

    fix: isbefore

--- date-is.js
+++ date-is.js
@@ -9,6 +9,6 @@ const isValid = (date) => {
     return true
 }
 const isAfter = (date_1, date_2) => new Date(date_1).getTime() > new Date(date_2).getTime()
-const isBefore = (date_1, date_2) => !isAfter(date_1, date_2)
+const isBefore = (date_1, date_2) => isAfter(date_2, date_1)
 const isFuture = (date) => isValid(date) && isAfter(date,new Date())
 const isPast = (date) => !isValid(date) && isBefore(date, new Date())
