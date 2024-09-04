f8f9eeef34ab9eee4c19d6aacabb80eb010ac489
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-04 11:17:38 +0300

    Fix: quantifiers.js

---
+++ quantifiers.js
@@ -0,0 +1,17 @@
+const  every=(array, test)=> {
+    for (var i = 0; i < array.length; i++) {
+        if (!tst(array[i])) return false;
+    }
+    return true;
+}
+
+const some = (array, tst) =>{
+    for (var i = 0; i < array.length; i++) {
+        if (tst(array[i])) return true;
+    }
+    return false;
+}
+
+const none = (array, tst) =>{
+    return !some(array, tst);
+}
