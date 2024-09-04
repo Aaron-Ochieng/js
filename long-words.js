4c4887eb491d9e5ecfa08476f416f8d679932177
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-04 08:43:30 +0300

    Fix: least

--- long-words.js
+++ long-words.js
@@ -1,5 +1,5 @@
 // const test = ['Junior','Aaron']
-const fiveonwards = (value) => typeof value === 'string' && value.length >= 5;
+const fiveonwards = (value) => typeof value === 'string' && value.length <= 5;
 const longWords = arr => arr.every(fiveonwards)
 // console.log(longWords(test))
 
@@ -8,5 +8,5 @@ const tencharonwards = (value) => value.length >= 10;
 const oneLongWord = arr => arr.some(tencharonwards)
 // console.log(oneLongWord(test))
 
-const ev = val => typeof val !== 'string' && val.length >= 7
+const ev = val => typeof val !== 'string' && val.length <= 7
 const noLongWords = arr => arr.every(ev)
