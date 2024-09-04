258c1303f70bfecaafbba7534981814dbfb75f7f
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-04 08:47:27 +0300

    fix: long words

--- long-words.js
+++ long-words.js
@@ -8,5 +8,5 @@ const tencharonwards = (value) => value.length >= 10;
 const oneLongWord = arr => arr.some(tencharonwards)
 // console.log(oneLongWord(test))
 
-const ev = val => typeof val !== 'string' && val.length <= 7
+const ev = val => typeof val !== 'string' && val.length < 7
 const noLongWords = arr => arr.every(ev)
