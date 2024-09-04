ae377954295f0b026f01a0a50dc90736e65a8d03
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-04 12:01:36 +0300

    Add:has-city.js

---
+++ has-city.js
@@ -0,0 +1,8 @@
+const hasCity=(country, cities)=> {
+    return function (city) {
+        if (cities.indexOf(city) === -1) {
+            return city + " is not a city from " + country;
+        }
+        return city + " is a city from " + country;
+    };
+}
