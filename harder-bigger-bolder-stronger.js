bca1e47ed1677a6140060146095360da0bb088c8
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-05 12:32:01 +0300

    Add: changing font sizes

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,17 @@
+export const generateLetters = () => {
+    for (let i = 1; i <= 120; i++) {
+        let letter = document.createElement("div");
+        letter.style.fontSize = `${11 + i}px`;
+        letter.textContent = String.fromCharCode(
+            65 + Math.floor(Math.random() * 26)
+        );
+        if (i < 40) {
+            letter.style.fontWeight = "300";
+        } else if (i < 80) {
+            letter.style.fontWeight = "400";
+        } else {
+            letter.style.fontWeight = "600";
+        }
+        document.getElementsByTagName("body")[0].appendChild(letter);
+    }
+}
