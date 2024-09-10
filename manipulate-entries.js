b487ef86af762ee758b174e0eab6ef933d9d4ce8
Aaron Ochieng <ochiengaoron@gmail.com>
2024-09-10 12:57:21 +0300

    Add: manipulate entries

---
+++ manipulate-entries.js
@@ -0,0 +1,63 @@
+const filterEntries = (obj, filter) => {
+    let res = {};
+    for (let key in obj) {
+        if (filter([key, obj[key]])) {
+            res[key] = obj[key];
+        }
+    }
+    return res;
+}
+
+const mapEntries = (entries, mapper) => {
+    let temp = {};
+    for (let key in entries) {
+        temp[key] = mapper([key, entries[key]]);
+    }
+    let res = {};
+    for (let key in temp) {
+        res[temp[key][0]] = temp[key][1];
+    }
+    return res;
+}
+
+const reduceEntries = (entries, reducer, initialValue) => {
+    let acc = initialValue;
+    for (let key in entries) {
+        acc = reducer(acc, [key, entries[key]]);
+    }
+    return acc;
+}
+
+const lowCarbs = (entries) => {
+    return filterEntries(entries, (entry) => {
+        let value = (nutritionDB[entry[0]]["carbs"] / 100) * entry[1];
+        return parseInt(value) <= 50;
+    });
+}
+
+const totalCalories = (entries) => {
+    return Number(
+        reduceEntries(
+            entries,
+            (acc, curr) => {
+                let value = (nutritionDB[curr[0]]["calories"] / 100) * curr[1];
+                return acc + value;
+            },
+            0
+        ).toFixed(1)
+    );
+}
+
+const cartTotal = (entries) => {
+    let res = {};
+    for (let key in entries) {
+        res[key] = {};
+        for (let dbKey in nutritionDB[key]) {
+            res[key][dbKey] =
+                Math.round(
+                    (entries[key] / 100) * nutritionDB[key][dbKey] * 1000
+                ) / 1000;
+        }
+    }
+    return res;
+}
